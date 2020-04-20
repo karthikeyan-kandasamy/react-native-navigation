af8a46816bb0f0d05aeb397c0e6a1b6844f89f03
karthi <>
2020-04-20 10:40:13 +0530

    Removed tests

--- lib/android/app/build/generated/source/buildConfig/reactNative56/release/com/reactnativenavigation/BuildConfig.java
+++ lib/android/app/build/generated/source/buildConfig/reactNative56/release/com/reactnativenavigation/BuildConfig.java
@@ -5,6 +5,11 @@ package com.reactnativenavigation;
 
 public final class BuildConfig {
   public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "com.reactnativenavigation";
+  /**
+   * @deprecated APPLICATION_ID is misleading in libraries. For the library package name use LIBRARY_PACKAGE_NAME
+   */
+  @Deprecated
   public static final String APPLICATION_ID = "com.reactnativenavigation";
   public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "reactNative56";
